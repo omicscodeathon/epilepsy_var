c11060624e3e4a25d33bb82fce1ec4176c1123e9
Modibo K Goita <41927092+mkgoita@users.noreply.github.com>
2023-02-14 00:51:06 +0000

    Update 1_buildGenomeIndex_star.sh

--- scripts/rnaseq/1_buildGenomeIndex_star.sh
+++ scripts/rnaseq/1_buildGenomeIndex_star.sh
@@ -8,7 +8,7 @@ REF_DIR="~/my_shared_data_folder/epilepsy/data/rnaseq/ref"
 # Indexing reference genome 
 samtools faidx ${REF_DIR}/GRCh38.fna
 
-NUM_BASES=`gawk '{sum = sum + \$2}END{if ((log(sum)/log(2))/2 - 1 > 14) {printf "%.0f", 14} else {printf "%.0f", (log(sum)/log(2))/2 - 1}}' GRCh38.fai`
+NUM_BASES=`gawk '{sum = sum + \$2}END{if ((log(sum)/log(2))/2 - 1 > 14) {printf "%.0f", 14} else {printf "%.0f", (log(sum)/log(2))/2 - 1}}' ${REF_DIR}/GRCh38.fai`
 
 
 # Run STAR genome generate (index)
