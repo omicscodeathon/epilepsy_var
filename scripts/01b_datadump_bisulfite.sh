c4f0b11561242c0048dd4c29829cd16db21b2ac2
Olaitan I. Awe <laitanawe@gmail.com>
2023-03-08 08:41:06 +0100

    Update scripts

---
+++ scripts/01b_datadump_bisulfite.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This script downloads fastq.qz files (bisulfite sequencing data)
+# Dependencies: fastq-dump
+
+SAMPLES=$(cat ../accessions/Bisulfite-data-accessions.txt)
+
+for SAMPLE in $SAMPLES; do
+
+ prefetch ${SAMPLE} --max-size u && fastq-dump --gzip --split-files ${SAMPLE} &&  rm -Rv ${SAMPLE}
+
+done
