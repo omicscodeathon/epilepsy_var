b61362531ffe06758a8d36caa78588728b83cc6c
marionnyaboke <45264074+marionnyaboke@users.noreply.github.com>
2023-05-25 22:36:02 +0300

    Update 08_diff-methylation.R

--- scripts/bisulfiteseq/08_diff-methylation.R
+++ scripts/bisulfiteseq/08_diff-methylation.R
@@ -128,14 +128,100 @@ myDiff25p <- myDiff25p[order(myDiff25p$qvalue),]
 
 ## CpG Annotation
 
-# First load the annotation data; i.e the coordinates of promoters, TSS, intron and exons
-refseq_anot <- readTranscriptFeatures("/srv/data/my_shared_data_folder/epilepsy/data/bisulfiteseq/ref/file.bed")
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+library(clusterProfiler)
 
-# Annotate hypermethylated CpGs ("target") with promoter/exon/intron
-# information ("feature"). This function operates on GRanges objects, so we # first coerce the methylKit object to GRanges.
-myDiff25p.hyper.anot <- annotateWithGeneParts(target = as(myDiff25p.hyper,"GRanges"),
-                                              feature = refseq_anot)
+files <- getSampleFiles()
+print(files)
 
-# Summary of target set annotation
-myDiff25p.hyper.anot
+peak <- readPeakFile(files[[4]])
+peak
+
+covplot(peak, weightCol="V5")
+
+covplot(peak, weightCol="V5", chrs=c("chr17", "chr18"), xlim=c(4.5e7, 5e7))
+
+data("tagMatrixList")
+tagMatrix <- tagMatrixList[[4]]
+
+tagHeatmap(tagMatrix, xlim=c(-3000, 3000), color="red")
+
+peakHeatmap(files[[4]], TxDb=txdb, upstream=3000, downstream=3000, color="red")
+
+plotAvgProf(tagMatrix, xlim=c(-3000, 3000),
+            xlab="Genomic Region (5'->3')", ylab = "Read Count Frequency")
+
+plotAvgProf2(files[[4]], TxDb=txdb, upstream=3000, downstream=3000,
+             xlab="Genomic Region (5'->3')", ylab = "Read Count Frequency")
+
+plotAvgProf(tagMatrix, xlim=c(-3000, 3000), conf = 0.95, resample = 1000)
+
+peakAnno <- annotatePeak(files[[4]], tssRegion=c(-3000, 3000),
+                         TxDb=txdb, annoDb="org.Hs.eg.db")
+
+plotAnnoPie(peakAnno)
+
+
+plotAnnoBar(peakAnno)
+
+upsetplot(peakAnno)
+
+plotDistToTSS(peakAnno,
+              title="Distribution of transcription factor-binding loci\nrelative to TSS")
+
+library(ReactomePA)
+
+pathway1 <- enrichPathway(as.data.frame(peakAnno)$geneId)
+head(pathway1, 2)
+
+data("tagMatrixList")
+plotAvgProf(tagMatrixList, xlim=c(-3000, 3000))
+
+plotAvgProf(tagMatrixList, xlim=c(-3000, 3000), conf=0.95,resample=500, facet="row")
+
+tagHeatmap(tagMatrixList, xlim=c(-3000, 3000), color=NULL)
+
+peakAnnoList <- lapply(files, annotatePeak, TxDb=txdb,
+                       tssRegion=c(-3000, 3000), verbose=FALSE)
+
+plotAnnoBar(peakAnnoList)
+
+plotDistToTSS(peakAnnoList)
+
+genes = lapply(peakAnnoList, function(i) as.data.frame(i)$geneId)
+names(genes) = sub("_", "\n", names(genes))
+compKEGG <- compareCluster(geneCluster   = genes,
+                           fun           = "enrichKEGG",
+                           pvalueCutoff  = 0.05,
+                           pAdjustMethod = "BH")
+dotplot(compKEGG, showCategory = 15, title = "KEGG Pathway Enrichment Analysis")
+
+genes= lapply(peakAnnoList, function(i) as.data.frame(i)$geneId)
+vennplot(genes)
+
+p <- GRanges(seqnames=c("chr1", "chr3"),
+             ranges=IRanges(start=c(1, 100), end=c(50, 130)))
+shuffle(p, TxDb=txdb)
+
+enrichPeakOverlap(queryPeak     = files[[5]],
+                  targetPeak    = unlist(files[1:4]),
+                  TxDb          = txdb,
+                  pAdjustMethod = "BH",
+                  nShuffle      = 50,
+                  chainFile     = NULL,
+                  verbose       = FALSE)
+
+getGEOspecies()
+
+getGEOgenomeVersion()
+
+hg19 <- getGEOInfo(genome="hg19", simplify=TRUE)
+head(hg19)
+
+downloadGEObedFiles(genome="hg19", destDir="hg19")
+
+gsm <- hg19$gsm[sample(nrow(hg19), 10)]
+downloadGSMbedFiles(gsm, destDir="hg19")
+
 
