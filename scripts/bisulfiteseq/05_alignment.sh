f7c3156e15d0308f54962d544412ab980edc89d6
George Eusebio Kuodza <83031200+dreusebio@users.noreply.github.com>
2024-03-22 22:45:23 -0700

    Update 05_alignment.sh
    changed from non-directional to directional because the mapping efficiency was high on the top strand(OT) and bottom strand(OB)

--- scripts/bisulfiteseq/05_alignment.sh
+++ scripts/bisulfiteseq/05_alignment.sh
@@ -4,7 +4,7 @@ SAMPLES="SRR10493738 SRR10493739 SRR10493740"
 
 for SAMPLE in $SAMPLES; do
 
-bismark --non_directional --genome ~/my_shared_data_folder/epilepsy/data/bisulfiteseq/ref -1 ${SAMPLE}_1_val_1.fq.gz -2 ${SAMPLE}_2_val_2.fq.gz -o output_directory
+bismark --parallel 2 --genome ~/my_shared_data_folder/epilepsy/data/bisulfiteseq/ref -1 ${SAMPLE}_1_val_1.fq.gz -2 ${SAMPLE}_2_val_2.fq.gz -o output_directory
 
 
 done
