f07bbe96058bb0f0c9ebde609eb8b916a13eae78
George Eusebio Kuodza <83031200+dreusebio@users.noreply.github.com>
2024-03-29 12:55:27 -0700

    Update 07_methylation_extractor.sh
    added some option bismark_methylation_extractor \
              	--paired-end \
              	--gzip \
              	--comprehensive \
              	--merge_non_CpG \
              	--bedGraph \
              	--multicore 6 \
              	--buffer_size 10G \

--- scripts/bisulfiteseq/07_methylation_extractor.sh
+++ scripts/bisulfiteseq/07_methylation_extractor.sh
@@ -6,6 +6,6 @@ SAMPLES="SRR10493737 SRR10493738 SRR10493739 SRR10493740"
 
 for SAMPLE in $SAMPLES; do
 
-bismark_methylation_extractor -p --merge_non_CpG --bedGraph --counts --buffer_size 10G ${SAMPLE}_1.fa_bismark_bt2_pe.bam
+bismark_methylation_extractor -p --gzip --comprehensive  --merge_non_CpG --bedGraph --multicore 6 --counts --buffer_size 10G ${SAMPLE}_1.fa_bismark_bt2_pe.bam
 
 done
