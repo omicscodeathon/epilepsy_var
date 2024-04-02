55aafc8c659932589bfd3e64084bbf73122fc00e
Jonathan Kalami <116915872+itsjoeka@users.noreply.github.com>
2024-04-02 09:48:44 +0000

    Update 06_Deduplication.sh

--- scripts/bisulfiteseq/06_Deduplication.sh
+++ scripts/bisulfiteseq/06_Deduplication.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/bash
 
+SAMPLES="SRR10493738 SRR10493739 SRR10493740"
+
+for SAMPLE in $SAMPLES; do
+
+deduplicate_bismark --parallel 2 --genome ~/my_shared_data_folder/epilepsy/data/bisulfiteseq/ref -1 ${SAMPLE}_1_val_1.fq.bam -2 ${SAMPLE}_2_val_2.fq.gz -o output_directory
+
+
+done
