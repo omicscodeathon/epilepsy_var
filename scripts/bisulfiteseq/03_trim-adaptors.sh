8e4d8a68ac399bf622752a2fbab35483086106fb
Jonathan Kalami <116915872+itsjoeka@users.noreply.github.com>
2024-04-17 18:19:21 +0000

    Update and rename 03_trim-adaptors-mRNA.sh to 03_trim-adaptors.sh

---
+++ scripts/bisulfiteseq/03_trim-adaptors.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+cd ~/my_shared_data_folder/epilepsy/data/bisulfiteseq
+
+mkdir -p ~/my_shared_data_folder/epilepsy/data/bisulfiteseq/results/trimmedseq
+
+FASTQ_DIR="~/my_shared_data_folder/epilepsy/data/bisulfiteseq"
+
+OUT_DIR="~/my_shared_data_folder/epilepsy/data/bisulfiteseq//results/trimmedseq"
+
+SAMPLES="SRR10493735 SRR10493736 SRR10493737 SRR10493738 SRR10493739 SRR10493740"
+
+for SAMPLE in $SAMPLES; do
+
+trim_galore --paired --cores 2 ${FASTQ_DIR}/${SAMPLE}_1.fastq.gz ${FASTQ_DIR}/${SAMPLE}_2.fastq.gz
+
+done
